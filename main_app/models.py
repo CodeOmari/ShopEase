2f06724a4d6f9cb7cb1d7474dc4d0a623b74a1a3
CodeOmari <jacksonomari2230@gmail.com>
2025-01-28 17:14:13 +0300

    added project models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    dob = models.DateField()
+    gender = models.CharField(max_length=6)
+    weight = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        db_table = "customers"
+
+
+class Transaction(models.Model):
+    item_type = models.CharField(max_length=100)
+    item_description = models.TextField()
+    item_quantity = models.IntegerField()
+    amount = models.IntegerField()
+    status = models.BooleanField(default=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.item_type
+
+    class Meta:
+        db_table = "transactions"
