69c81161fa999bac022f111fffbed50cbee77e2a
CodeOmari <jacksonomari2230@gmail.com>
2025-01-31 14:29:59 +0300

    forms file

---
+++ main_app/app_forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from main_app.models import Customer
+
+GENDER_CHOICES = {"Male": "Male", "Female": "Female"}
+class CustomerForm(forms.ModelForm):
+    gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.RadioSelect)
+    class Meta:
+        model = Customer
+        fields = ['first_name', 'last_name', 'email', 'dob', 'gender', 'weight']
+        widgets = {
+            'dob' : forms.DateInput(attrs={'type': 'date', 'min':'1980-01-01', 'max':'2005-12-31'}),
+            'weight': forms.NumberInput(attrs={'type': 'number', 'min':'35', 'max':'100'})
+        }
