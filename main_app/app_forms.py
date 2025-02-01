06e8c606d78327d6b70421f0cbe7f42a830ebca8
CodeOmari <jacksonomari2230@gmail.com>
2025-02-01 09:09:59 +0300

    added a login form

--- main_app/app_forms.py
+++ main_app/app_forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from main_app.models import Customer
+from main_app.models import Customer, Transaction
 
 GENDER_CHOICES = {"Male": "Male", "Female": "Female"}
 class CustomerForm(forms.ModelForm):
@@ -12,3 +12,7 @@ class CustomerForm(forms.ModelForm):
             'dob' : forms.DateInput(attrs={'type': 'date', 'min':'1980-01-01', 'max':'2005-12-31'}),
             'weight': forms.NumberInput(attrs={'type': 'number', 'min':'35', 'max':'100'})
         }
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput)
