bd17b11f4ef3b8dd391fd8394c8d0597d5ab62e3
CodeOmari <jacksonomari2230@gmail.com>
2025-02-01 09:09:46 +0300

    added projects admin panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+from main_app.models import Customer, Transaction
 
 # Register your models here.
+
+admin.site.site_header = 'ShopEase Administration'
+
+admin.site.site_title = 'ShopEase Administration'
+
+admin.site.register(Customer)
+admin.site.register(Transaction)
