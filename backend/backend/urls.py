d2161e5dbb2ee657ee05e3096e697a5be04ccc3e
CodeOmari <jacksonomari2230@gmail.com>
2026-08-03 20:17:09 +0300

    update the URL routing configuration to include the accounts app URLs

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from backend import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path("api/", include("user_management.urls")),
 
+    path("accounts/", include("accounts.urls")),
+    
     path("shopease/", include("shop.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
