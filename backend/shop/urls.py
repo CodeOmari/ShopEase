0b78b988fed24239f46bf6618b08fb7c7c3c3d74
CodeOmari <jacksonomari2230@gmail.com>
2026-08-06 12:12:28 +0300

    add URL Routing configuration for the shop app

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -1,5 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, ProductViewSet, ProductImageViewSet, ReviewViewSet
+
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'product-images', ProductImageViewSet)
+router.register(r'reviews', ReviewViewSet)
 
 
 urlpatterns = [
+    path('', include(router.urls))
 ]
