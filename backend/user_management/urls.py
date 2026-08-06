a07231801d44e461f44a0371df71233609918a3b
CodeOmari <jacksonomari2230@gmail.com>
2026-08-06 12:15:31 +0300

    update URL routing configuration for the user registration and login app

--- backend/user_management/urls.py
+++ backend/user_management/urls.py
@@ -4,11 +4,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
-    path('user/register/', CustomUserCreateView.as_view(), name='register'),
+    path('register/', CustomUserCreateView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='get_token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='refresh'),
 
-    path('user/', current_user),
+    path('current-user/', current_user),
     
     path('api-auth/', include('rest_framework.urls')),
 ]
