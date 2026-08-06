116d8e205daa68317f1c67e5c2e392e223d60a9e
CodeOmari <jacksonomari2230@gmail.com>
2026-08-06 12:04:28 +0300

    add URL routing configuration for the customer and seller profiles

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from .views import CustomerProfileViewSet, SellerProfileViewSet
+
+router = DefaultRouter()
+router.register(r'customer-profile', CustomerProfileViewSet)
+router.register(r'seller-profile', SellerProfileViewSet)
 
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
