a04051ca105712b693b1cb44117e266bbe7beff3
CodeOmari <jacksonomari2230@gmail.com>
2026-08-06 12:05:04 +0300

    add views for the customer and seller profiles

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .serializers import CustomerProfileSerializer, SellerProfileSerializer
+from .models import CustomerProfile, SellerProfile
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+class CustomerProfileViewSet(viewsets.ModelViewSet):
+    queryset = CustomerProfile.objects.all()
+    serializer_class = CustomerProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return CustomerProfile.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class SellerProfileViewSet(viewsets.ModelViewSet):
+    queryset = SellerProfile.objects.all()
+    serializer_class = SellerProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return SellerProfile.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
