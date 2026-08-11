5ac9524acb971b63ed6f79af66e15c1192dbbc57
CodeOmari <jacksonomari2230@gmail.com>
2026-08-11 10:47:01 +0300

    add permission class for the wishlist viewsets

---
+++ backend/wishlist/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission
+
+class IsCustomer(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and hasattr(request.user, "customer_profile")
+        )
