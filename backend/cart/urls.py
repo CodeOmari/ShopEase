40cdc2369ac8a96e1b219997a456d5208704dc53
CodeOmari <jacksonomari2230@gmail.com>
2026-08-11 10:41:59 +0300

    add URL routing configuration for the cart app

---
+++ backend/cart/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import CartItemViewSet, CartViewSet, OrderViewSet, OrderItemViewSet
+
+router = DefaultRouter()
+router.register(r'carts', CartViewSet)
+router.register(r'cart-items', CartItemViewSet)
+router.register(r'orders', OrderViewSet)
+router.register(r'order-items', OrderItemViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
