1c8c14322691e1b5a0006c8fb72aa53c03d41c86
CodeOmari <jacksonomari2230@gmail.com>
2026-08-11 10:37:07 +0300

    add serializer class for the new customer address model

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,4 +1,4 @@
-from .models import CustomerProfile, SellerProfile
+from .models import CustomerProfile, SellerProfile, CustomerAddress
 from rest_framework import serializers
 
 class CustomerProfileSerializer(serializers.ModelSerializer):
@@ -7,6 +7,25 @@ class CustomerProfileSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ['id', 'user', 'created_at', 'updated_at']
 
+class CustomerAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerAddress
+        field = [
+            "id",
+            "label", 
+            "shipping_address", 
+            "street", 
+            "county", 
+            "postal_code", 
+            "is_default", 
+            "created_at", 
+            "updated_at"
+        ]
+        read_only_fields = [
+            "id",
+            "created_at", 
+            "updated_at",
+        ]
 
 class SellerProfileSerializer(serializers.ModelSerializer):
     class Meta:
