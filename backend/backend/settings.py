501663c4c90a51151b2757534c6ce7601192f818
CodeOmari <jacksonomari2230@gmail.com>
2026-09-11 17:19:15 +0300

    update accounts app naming to ensure their signals are triggered accurately

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'user_management',
-    'accounts',
+    'accounts.apps.AccountsConfig',
     'shop',
     'cart',
     'wishlist',
