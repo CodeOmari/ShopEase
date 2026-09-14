2fd89b841d08438e4b2f0cfa5c6d0079a3bb51dc
CodeOmari <jacksonomari2230@gmail.com>
2026-09-14 16:03:10 +0300

    add signal for automatically creating an admin profile account

--- backend/accounts/signals.py
+++ backend/accounts/signals.py
@@ -1,16 +1,24 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from user_management.models import CustomUser
-from .models import CustomerProfile, SellerProfile
+from .models import CustomerProfile, SellerProfile, AdminProfile
 
 
 @receiver(post_save, sender=CustomUser)
 def create_customer_profile(sender, instance, created, **kwargs):
-    if created:
+    if created and instance.role == "BUYER":
         CustomerProfile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=CustomUser)
 def create_seller_profile(sender, instance, created, **kwargs):
-    if created:
-        SellerProfile.objects.create(user=instance)
+    if created and instance.role == "SELLER":
+        SellerProfile.objects.create(
+            user=instance,
+            business_email=instance.email
+        )
+
+@receiver(post_save, sender=CustomUser)
+def create_admin_profile(sender, instance, created, **kwargs):
+    if created and instance.role == "ADMIN":
+        AdminProfile.objects.create(user=instance)
