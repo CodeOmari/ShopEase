12b08483a087f12d259eeb6be04c4fa867586f1c
CodeOmari <jacksonomari2230@gmail.com>
2026-09-14 16:04:17 +0300

    add url router configuration for the admin profile

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
-from .views import CustomerProfileViewSet, SellerProfileViewSet
+from .views import CustomerProfileViewSet, SellerProfileViewSet, AdminProfileViewSet
 
 router = DefaultRouter()
 router.register(r'customer-profile', CustomerProfileViewSet)
 router.register(r'seller-profile', SellerProfileViewSet)
-
+router.register(r'admin-profile', AdminProfileViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
