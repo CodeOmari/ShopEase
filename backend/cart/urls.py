45fce986ed4eab7ffe375942a1bf8bb108951e55
CodeOmari <jacksonomari2230@gmail.com>
2026-09-17 13:27:40 +0300

    update the url routing configuration for the shopping cart

--- backend/cart/urls.py
+++ backend/cart/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import CartItemViewSet, CartViewSet, OrderViewSet, OrderItemViewSet
 
 router = DefaultRouter()
-router.register(r'carts', CartViewSet)
+router.register(r'shopping-cart', CartViewSet)
 router.register(r'cart-items', CartItemViewSet)
 router.register(r'orders', OrderViewSet)
 router.register(r'order-items', OrderItemViewSet)
