5b12d92c9b02dca41d36222fff9c060419e36b34
CodeOmari <jacksonomari2230@gmail.com>
2026-09-24 17:58:39 +0300

    add data file containing products

---
+++ frontend/src/products.js
@@ -0,0 +1,35 @@
+import Headphones from './assets/aurora-headphones.jpg';
+import Keyboard from './assets/keyboard.jpg';
+import Backpack from './assets/leather-backpack.jpg';
+import Smartwatch from './assets/titan-smartwatch.jpg';
+
+export default [
+    {
+        id:1,
+        img: {
+            src: Headphones,
+            alt: 'Aurora Headphones'
+        },
+    },
+    {
+        id:2,
+        img: {
+            src: Keyboard,
+            alt: 'Metallic keyboard'
+        }
+    },
+    {
+        id:3,
+        img: {
+            src: Backpack,
+            alt: 'Leather Backpack'
+        }
+    },
+    {
+        id: 4,
+        img: {
+            src: Smartwatch,
+            alt: 'Titan Smartwatch'
+        }
+    },
+]
